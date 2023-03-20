385a75029a78b1a15ff58f9ed32bc4af9d9f5e43
Marcio Woitek <mwoitekjr@gmail.com>
2023-03-20 14:39:34 -0300

    better key generation

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,16 +1,17 @@
 import { client } from '$services/redis';
+import { pageCacheKey } from '$services/keys';
 
 const cacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signup'];
 
 export const getCachedPage = (route: string) => {
 	if (cacheRoutes.includes(route)) {
-		return client.get(`pagecache#${route}`);
+		return client.get(pageCacheKey(route));
 	}
 	return null;
 };
 
 export const setCachedPage = (route: string, page: string) => {
 	if (cacheRoutes.includes(route)) {
-		return client.set(`pagecache#${route}`, page, { EX: 2 });
+		return client.set(pageCacheKey(route), page, { EX: 2 });
 	}
 };
