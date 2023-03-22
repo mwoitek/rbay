3789fc929c509202b98023fee35eb04c42fc3610
Marcio Woitek <mwoitekjr@gmail.com>
2023-03-22 15:13:05 -0300

    sandbox: multiple commands at the same time

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,22 +2,30 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1950
-		// engine: { cylinders: 8 },
-		// owner: null || '',
-		// service: undefined || ''
+	});
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
 	});
 
-	// const car = await client.hGetAll('car#352352352');
-	const car = await client.hGetAll('car');
+	const commands = [1, 2, 3].map((id) => {
+		return client.hGetAll('car' + id);
+	});
 
-	if (Object.keys(car).length === 0) {
-		console.log('Car not found, respond with 404');
-		return;
-	}
-	console.log(car);
+	const results = await Promise.all(commands);
+	// const results = await Promise.all([
+	// 	client.hGetAll('car1'),
+	// 	client.hGetAll('car2'),
+	// 	client.hGetAll('car3')
+	// ]);
+	console.log(results);
 };
 
 run();
