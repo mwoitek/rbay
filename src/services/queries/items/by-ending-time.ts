e3da40b4b779addd982483af70e40823ed0ce582
Marcio Woitek <mwoitekjr@gmail.com>
2023-03-23 18:14:15 -0300

    finish "Ending Soonest"

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,11 +1,11 @@
 import { client } from '$services/redis';
-import { itemsKey, itemsByEndingAtKey } from '$services/keys';
-import { deserialize } from './deserialize';
+import { itemsByEndingAtKey } from '$services/keys';
+import { getItems } from './items';
 
 export const itemsByEndingTime = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
 	const ids = await client.zRange(itemsByEndingAtKey(), Date.now(), '+inf', {
 		BY: 'SCORE',
 		LIMIT: { offset, count }
 	});
-	console.log(ids);
+	return getItems(ids);
 };
