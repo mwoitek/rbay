a8eabfc0af85e6480b198de768f609f8779f573f
Marcio Woitek <mwoitekjr@gmail.com>
2023-03-30 12:52:28 -0300

    Use Lua script to avoid accidental unlocks

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -40,6 +40,20 @@ const client = createClient({
 				return [itemsViewsKey(itemId), itemsKey(itemId), itemsByViewsKey(), itemId, userId];
 			},
 			transformReply() {}
+		}),
+		unlock: defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT: `
+				if redis.call('GET', KEYS[1]) == ARGV[1] then
+					return redis.call('DEL', KEYS[1])
+				end
+			`,
+			transformArguments(key: string, token: string) {
+				return [key, token];
+			},
+			transformReply(reply: any) {
+				return reply;
+			}
 		})
 	}
 });
