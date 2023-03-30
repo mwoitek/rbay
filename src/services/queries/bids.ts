311c2066b1894fdfc286fb68fc4c5f3fb2444e9e
Marcio Woitek <mwoitekjr@gmail.com>
2023-03-30 12:06:02 -0300

    Add lock to bid creation

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,13 +1,11 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
-import { client } from '$services/redis';
+import { client, withLock } from '$services/redis';
 import { bidHistoryKey, itemsKey, itemsByPriceKey } from '$services/keys';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	return client.executeIsolated(async (isolatedClient) => {
-		await isolatedClient.watch(itemsKey(attrs.itemId));
-
+	return withLock(attrs.itemId, async () => {
 		const item = await getItem(attrs.itemId);
 		if (!item) {
 			throw new Error('Item does not exist');
@@ -21,19 +19,18 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 
 		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
 
-		return isolatedClient
-			.multi()
-			.rPush(bidHistoryKey(attrs.itemId), serialized)
-			.hSet(itemsKey(item.id), {
+		return Promise.all([
+			client.rPush(bidHistoryKey(attrs.itemId), serialized),
+			client.hSet(itemsKey(item.id), {
 				bids: item.bids + 1,
 				price: attrs.amount,
 				highestBidUserId: attrs.userId
-			})
-			.zAdd(itemsByPriceKey(), {
+			}),
+			client.zAdd(itemsByPriceKey(), {
 				value: item.id,
 				score: attrs.amount
 			})
-			.exec();
+		]);
 	});
 };
 
